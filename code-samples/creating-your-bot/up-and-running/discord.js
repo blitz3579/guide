f7355fa55de0f92c3e45f3ee10a209d0faa66ae9
blitz3579 <59101657+blitz3579@users.noreply.github.com>
2020-03-13 18:17:23 -0400

    discordbot.js

---
+++ code-samples/creating-your-bot/up-and-running/discord.js
@@ -0,0 +1,12 @@
+const Discord = require('discord.js');
+const client = new Discord.Client();
+
+client.once('ready', () => {
+	console.log('Ready!');
+});
+client.on('message', message => {
+if (message.content === '!ping') {
+	// send back "Pong." to the channel the message was sent in
+	message.channel.send('Pong.');
+}
+client.login('Njg3MDIyMjIyNTIyNDUwMDEy.XmwFwA.NEmI2GvpjMVEpxbMv1qhRRJjdCE');
